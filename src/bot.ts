e48418645b490f920967d9bde330d9f783272e10
johnmeshulam <johnmeshulam@gmail.com>
2020-09-25 16:33:42 +0300

    connect to db and then discord

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,8 @@
 import { DiscordBot } from "./DiscordBot";
 import { DBManager } from "./db/db";
-import { resolve } from "path";
 
 const bot = DiscordBot.getInstance();
-bot.connect();
-DBManager.getConnection();
+
+DBManager.getConnection().then(() => {
+  bot.connect();
+});
