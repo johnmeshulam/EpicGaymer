080caddf0a6b66b11765f328aa7194511b211f0f
johnmeshulam <johnmeshulam@gmail.com>
2020-09-26 18:04:03 +0300

    update getValue

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,10 @@ export default class Config {
   private static options: Map<string, string>;
   private static service = new ConfigurationService();
 
-  public static getValue(key: string) {
-    return this.options.get(key);
+  public static getValue(key: string): string {
+    const value = this.options.get(key);
+    if (value) return value;
+    throw new Error(`Could not get config value for key ${key}`); //TODO throw custom exception
   }
 
   public static fetchValues(): void {
