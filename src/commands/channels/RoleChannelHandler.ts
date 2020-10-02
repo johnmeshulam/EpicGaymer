c71b07caa7553976a56920772d3ff566e2d95317
johnmeshulam <johnmeshulam@gmail.com>
2020-10-03 01:43:43 +0300

    add accept and give commands

--- src/commands/channels/RoleChannelHandler.ts
+++ src/commands/channels/RoleChannelHandler.ts
@@ -1,18 +1,93 @@
-import { Message } from "discord.js";
-import { parseCommand } from "../textUtils";
+import { Message, Guild } from "discord.js";
+import Config from "../../config";
+import GuildService from "../../guild/GuildService";
+import MemberService from "../../guild/MemberService";
+import AllowedRoles from "../../roles";
+import { parseCommand } from "../../textUtils";
 
 export class RoleChannelHandler {
   public static handleMessage(message: Message): void {
     const content = parseCommand(message.content);
-
-    if (content.command === "give") this.giveRoleCommand(message, content.args);
-    if (content.command === "revoke") return;
+    switch (content.command.toLowerCase()) {
+      case "accept":
+        this.acceptRulesCommand(message);
+        break;
+      case "give":
+        this.giveRoleCommand(message, content.args);
+        break;
+      default:
+        break;
+    }
   }
 
-  private static giveRoleCommand(message: Message, args: string): void {
-    //check permissions - no == msg and return
-    //check if role is requestable - no == reaction and return
-    //check if member has role - yes == msg and return
-    //give the role - success == msg and return
+  private static giveRoleCommand(message: Message, name: string): void {
+    try {
+      if (!message.member || !message.guild) return; //Need this for strong types even though its checked before
+
+      if (!MemberService.hasMemberRole(message.member)) {
+        message.reply(this.needToAcceptText(message.guild));
+        return;
+      }
+
+      if (!name) {
+        message.reply(this.needRoleNameMessage);
+        return;
+      }
+
+      if (!AllowedRoles.hasRole(name)) {
+        message.react("🚫");
+        return;
+      }
+
+      if (MemberService.hasRole(message.member, name)) {
+        message.reply(this.alreadyHasRoleText(name));
+        return;
+      }
+
+      MemberService.giveRole(
+        message.member,
+        GuildService.getRole(message.guild, AllowedRoles.getRoleName(name))
+      );
+      message.react("👍");
+    } catch (error) {
+      message.react("⚠");
+      console.error(error.message);
+    }
   }
+
+  private static acceptRulesCommand(message: Message) {
+    try {
+      if (!message.member || !message.guild) return;
+
+      if (MemberService.hasMemberRole(message.member)) {
+        message.reply(this.alreadyAcceptedText);
+        return;
+      }
+
+      MemberService.giveRole(
+        message.member,
+        GuildService.getRole(message.guild, "member")
+      );
+      message.react("👍");
+    } catch (error) {
+      message.react("⚠");
+      console.error(error.message);
+    }
+  }
+
+  private static needRoleNameMessage =
+    "Please type the name of the role you are requesting!";
+
+  private static needToAcceptText(guild: Guild): string {
+    return `Please accept the rules before requesting a game role! After reading ${GuildService.getChannel(
+      guild,
+      "rules"
+    ).toString()} type \`${Config.getValue("prefix")}accept\` to accept.`;
+  }
+
+  private static alreadyHasRoleText(name: string): string {
+    return `You already have the role ${AllowedRoles.getRoleName(name)}!`;
+  }
+
+  private static alreadyAcceptedText = "You have already accepted the rules!";
 }
