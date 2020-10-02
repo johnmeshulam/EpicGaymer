446b1f5894704db432cc246c7709bdab4f15fc27
johnmeshulam <johnmeshulam@gmail.com>
2020-10-03 01:42:44 +0300

    create file

---
+++ src/roles.ts
@@ -0,0 +1,26 @@
+import RoleService from "./db/roles/RoleService";
+import { RoleNotFoundException } from "./exceptions";
+import { createRoleIdentifier } from "./textUtils";
+
+export default class AllowedRoles {
+  private static roles: Map<string, string>;
+  private static service = new RoleService();
+
+  public static hasRole(name: string): boolean {
+    return this.roles.has(createRoleIdentifier(name));
+  }
+
+  public static getRoleName(name: string): string {
+    const identifier = createRoleIdentifier(name);
+    const roleName = this.roles.get(identifier);
+    if (!roleName) throw new RoleNotFoundException(identifier);
+    return roleName;
+  }
+
+  public static fetchValues(): void {
+    this.service.mapValues().then((result) => {
+      this.roles = result;
+      console.log("Fetched allowed roled from database!");
+    });
+  }
+}
