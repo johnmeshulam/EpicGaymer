6702832a5a71cd0892943450a83cdf8048b19cc2
johnmeshulam <johnmeshulam@gmail.com>
2020-10-03 01:42:52 +0300

    create file

---
+++ src/guild/MemberService.ts
@@ -0,0 +1,20 @@
+import { GuildMember, Role } from "discord.js";
+import { createRoleIdentifier } from "../textUtils";
+
+export default class MemberService {
+  static hasRole(member: GuildMember, name: string): boolean {
+    const role = member.roles.cache.find(
+      (role) => createRoleIdentifier(role.name) === createRoleIdentifier(name)
+    );
+    if (!role) return false;
+    return true;
+  }
+
+  static hasMemberRole(member: GuildMember): boolean {
+    return this.hasRole(member, "member");
+  }
+
+  static giveRole(member: GuildMember, role: Role): void {
+    member.roles.add(role);
+  }
+}
