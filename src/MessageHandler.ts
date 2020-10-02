1a680f2025cf0bc5033d36b5c5ef374a2b6c88a2
johnmeshulam <johnmeshulam@gmail.com>
2020-10-03 01:43:08 +0300

    update to switch case

--- src/MessageHandler.ts
+++ src/MessageHandler.ts
@@ -4,12 +4,14 @@ import { RoleChannelHandler } from "./commands/channels/RoleChannelHandler";
 
 export class MessageHandler {
   static handle(message: Message): void {
-    //Currently only handles server text channels
+    //Currently only handles guild text channels
     if (message.channel.type !== "text") return;
     if (!isCommand(message)) return;
 
-    if (message.channel.name === Config.getValue("role-channel"))
-      RoleChannelHandler.handleMessage(message);
+    switch (message.channel.name) {
+      case Config.getValue("role-channel"):
+        RoleChannelHandler.handleMessage(message);
+    }
   }
 }
 
