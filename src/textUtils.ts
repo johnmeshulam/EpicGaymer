736bf76fd057e2113f960f73ce4d8711a18ba8cc
johnmeshulam <johnmeshulam@gmail.com>
2020-10-03 01:43:22 +0300

    add parsing to commands with no args

--- src/textUtils.ts
+++ src/textUtils.ts
@@ -1,6 +1,8 @@
 export function parseCommand(
   content: string
 ): { command: string; args: string } {
+  if (content.indexOf(" ") === -1)
+    return { command: content.substring(1), args: "" };
   return {
     command: content.substring(1, content.indexOf(" ")),
     args: content.substring(content.indexOf(" ") + 1)
