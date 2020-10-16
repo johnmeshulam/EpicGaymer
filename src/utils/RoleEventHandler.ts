79498251e74db81fbbc06cdd7825f3d2a54a9bf8
johnmeshulam <johnmeshulam@gmail.com>
2020-10-17 00:14:19 +0300

    new class for handling event roles

---
+++ src/utils/RoleEventHandler.ts
@@ -0,0 +1,21 @@
+import { Guild, Role } from "discord.js";
+import RoleService from "../db/roles/RoleService";
+
+export default class RoleEventHandler {
+  private static service = new RoleService();
+
+  public static handleDeleteEvent(role: Role) {
+    this.service.deleteRole(role);
+    //TODO: update rules msg
+  }
+
+  public static handleUpdateEvent(role: Role) {
+    this.service.updateRole(role);
+    //TODO: update rules msg
+  }
+
+  public static handleCreateEvent(role: Role) {
+    this.service.createRole(role);
+    //TODO: update rules msg
+  }
+}
