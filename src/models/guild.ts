884f65454e819b2e157d555aed0cba9cbc8e7126
johnmeshulam <johnmeshulam@gmail.com>
2020-10-18 16:32:10 +0300

    add rules entry

--- src/models/guild.ts
+++ src/models/guild.ts
@@ -5,4 +5,10 @@ export default interface GuildEntry {
   identifier: string;
   roles: Array<RoleEntry>;
   configuration: Array<ConfigurationEntry>;
+  rules: RulesEntry;
+}
+
+export interface RulesEntry {
+  channel: string;
+  messageId: string;
 }
